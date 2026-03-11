898190624c4aed49915010cddc60b3b15dad6e69
zeka <zeka.huseynliwork@gmail.com>
2026-03-11 22:23:41 +0400

    fixed

--- Program.cs
+++ Program.cs
@@ -1,102 +1,4 @@
-﻿//metod - a n dəyəri göndəriləcək.Metod həmin ədədin kvadratını qaytaracaq
-
-//static int a(int n)
-//{
-//    return n * n;
-//}
-
-//int netice = a(5);
-//Console.WriteLine(netice);
-
-//Metod - a n dəyəri göndəriləcək.metod 1-dən n-dək olan cüt ədədlərin cəmini qaytaracaq
-
-//static int a(int n)
-//{
-//    int cem = 0;
-//    for (int i = 1; i <= n; i++)
-//    {
-//        if (i % 2 == 0)
-//        {
-//            cem += i;
-//        }
-//    }
-//    return cem;
-//}
-//int netice = a(10);
-//Console.WriteLine(netice);
-
-//metod - a ədədlər massiv göndəriləcək.Həmin massivdəki, ən kiçik elementi tapıb geri qaytaracaq
-
-//static int a(int[] ededler)
-//{
-//    int eded = ededler[0];
-//    for (int i = 1; i < ededler.Length; i++)
-//    {
-//        if (eded > ededler[i])
-//        {
-//            eded = ededler[i];
-//        }
-//    }
-//    return eded;
-//}
-//int[] ededler = { 5, 3, 8, 1, 4 };
-//int netice = a(ededler);
-//Console.WriteLine(netice);
-
-//Metod - a söz və hərf göndəriləcək. Həmin sözdə verilən hərfdən neçə ədəd olduğunu qaytaracaq metod
-
-//static int a(string soz, char herf)
-//{
-//    int eded = 0;
-//    for (int i = 0; i < soz.Length; i++)
-//    {
-//        if (soz[i] == herf)
-//        {
-//            eded++;
-//        }
-//    }
-//    return eded;
-//}
-
-//string soz = "aabdbdcbasjdlqwa";
-//int netice = a(soz, 'a');
-//Console.WriteLine(netice);
-
-//Metod - a n və m ədədləri göndəriləcək. Həmin n və m ededleri arasindaki tek ededlerin sayini tapin.
-
-//static int a(int n, int m)
-//{
-//    int say = 0;
-//    for (int i = n; i < m; i++)
-//    {
-//        if (i % 2 == 1)
-//        {
-//            say++;
-//        }
-//    }
-//    return say;
-//}
-//int netice = a(1, 10);
-//Console.WriteLine(netice);
-
-//metod - a bir string dəyər göndəriləcək. Həmin stringin polindrom olub olmamasini (true/false) qaytaracaq metod 
-
-//static bool a(string soz)
-//{
-//    string ters = "";
-//    for (int i = soz.Length - 1; i >= 0; i--)
-//    {
-//        ters += soz[i];
-//    }
-//    return soz == ters;
-//}
-//string soz = "ada";
-//Console.WriteLine(a(soz));
-//(polindrom: özü və tərs oxunuşu eyni olan sözlərdir (mühüm, kiçik))
-
-//Metod-a 2 ədəd göndəriləcək və üzərində verilmiş simvola uyğun əməliyyat edən proqram. Simvol +,-,* və ya / olmalıdır, əks halda simvolu yenidən daxil edilməsi istənilməlidir.(Misaçün simvol * olarsa verilmiş 2 ədəd vurulacaq və nəticəsi göstəriləcək)
-
-
+﻿//1
 //static double a(double n, double m, char simvol)
 //{
 //    if (simvol == '+')
@@ -124,21 +26,66 @@
 //double netice = a(10, 5, '*');
 //Console.WriteLine(netice);
 
+//Metod - a n dəyəri göndəriləcək.metod 1-dən n-dək olan cüt ədədlərin cəmini qaytaracaq
 
-//Parametr olaraq 1 string dəyər qəbul edən və həmin string dəyəri icində bosluqlar qalmayacaq hala gətirən metod.Misal olaraq, name = " Hikmet  Abbasov " deyə bir variable-mız varsa onu yaratdigimiz metoda göndərdikdə variable-daki dəyər "HikmətAbbasov" olmalıdır.
+//2.Verilen arqumentlere uygun tek ve cut edeleri tapan method yazin.(14, 20, 35, 40, 57, 60, 100)
 
-//static string a(string soz)
+//static void a(int[] ededler, int[] tek, int[] cut)
 //{
-//    string netice = "";
-//    for (int i = 0; i < soz.Length; i++)
-//    {`
-//        if (soz[i] != ' ')
+//    int t = 0;
+//    int c = 0;
+
+//    for (int i = 0; i < ededler.Length; i++)
+//    {
+//        if (ededler[i] % 2 == 0)
 //        {
-//            netice += soz[i];
+//            cut[c++] = ededler[i];
+//        }
+//        else
+//        {
+//            tek[c++] = ededler[i];
 //        }
 //    }
-//    return netice;
 //}
-//string name = " Hikmet  Abbasov ";
-//string netice = a(name);
-//Console.WriteLine(netice);
+//int[] ededler = { 14, 20, 35, 40, 57, 60, 100 };
+//int[] tek = new int[ededler.Length];
+//int[] cut = new int[ededler.Length];
+//a(ededler, tek, cut);
+
+//Console.WriteLine(string.Join(" ", tek));
+//Console.WriteLine(string.Join(" ", cut));
+
+//3.Verilmis arreyde elementlerin həm 4-ə, həm də 5-ə bölününen elementlerin cemini tapin.[14, 20, 35, 40, 57, 60, 100]
+
+//static void a(int[] ededler)
+//{
+//    int cem = 0;
+//    for (int i = 0; i < ededler.Length; i++)
+//    {
+//        if (ededler[i] % 4 == 0 && ededler[i] % 5 == 0)
+//        {
+//            cem += ededler[i];
+//        }
+//    }
+//    Console.WriteLine(cem);
+//}
+//int[] ededler = { 14, 20, 35, 40, 57, 60, 100 };
+//a(ededler);
+
+//4.Daxil edilmiş cümlədə daxil edilmis simvoldan nece eded olduğunu tapan Proqramın alqoritmini yazin.(Cumle serbestdir).
+
+static void a(string soz, char herf)
+{
+    int eded = 0;
+    for (int i = 0; i < soz.Length; i++)
+    {
+        if (soz[i] == herf)
+        {
+            eded++;
+        }
+    }
+    Console.WriteLine(eded);
+}
+
+string soz = "aabdbdcbasjdlqwa";
+a(soz, 'b');
